af3b900953d03ea6190bf515e5d8edc930001409
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-12-12 22:17:34 +0545

    First Commit

---
+++ src/components/Registration.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div `
+
+`
+
+
+export default function Registration() {
+    return (
+        <Container>
+            <Styles>
+                <h1 class="text-center">React Hook Registration Form</h1>
+                
+            </Styles>
+        </Container>
+    )
+}
